5be45f2f436dbf543cbc38606c4e505fa588fbc1
Zhanna Arobelidze <arobelidzezhuzhuna@gmail.com>
2023-05-09 21:40:45 +0300

    + quick, counting, radix, heap, shell

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -1,3 +1,5 @@
+from math import *
+
 def bubble_sort(lst):
     #Time Complexity
     #Best               O(n)
@@ -76,8 +78,109 @@ def merge_sort(lst):
             k += 1
     return lst
 
-def quicksort(lst):
+def partition(lst, start, stop):
+    more_index = start - 1
+    for j in range(start, stop):
+        if lst[j] <= lst[stop]:
+            more_index = more_index + 1
+            if j != more_index:
+                lst[more_index], lst[j] = lst[j], lst[more_index]
+    lst[more_index + 1], lst[stop] = lst[stop], lst[more_index + 1]
+    return more_index + 1
 
+def quick_sort(lst, start, stop):
+    # Time Complexity
+    # Best               O(n*log n)
+    # Worst              O(n^2)
+    # Average            O(n*log n)
+    # Space Complexity   O(log n)
+    # Stability	         NO
+    if start < stop:
+        pivot = partition(lst, start, stop)
+        quick_sort(lst, start, pivot - 1)
+        quick_sort(lst, pivot + 1, stop)
+    return lst
 
-list = [6, 5, 3, 1, 8, 7, 2, 4]
-print(merge_sort(list))
+def counting_sort(lst):
+    # Time Complexity
+    # Best               O(n+k)
+    # Worst              O(n+k)
+    # Average            O(n+k)
+    # Space Complexity   O(max)
+    # Stability	         YES
+    count_elem = {}
+    for i in lst:
+        count_elem[i] = count_elem.get(i, 0) + 1
+    res = []
+    for i,j in count_elem.items():
+        res.extend([i] * j)
+    return res
+
+def radix_sort(lst):
+    # Time Complexity
+    # Best               O(n+k)
+    # Worst              O(n+k)
+    # Average            O(n+k)
+    # Space Complexity   O(max)
+    # Stability	         YES
+    maximum, place = max(lst), 1
+    while maximum // place > 0:
+        output, count = [0] * len(lst), [0] * 10
+        for i in lst:
+            index = i // place
+            count[index % 10] += 1
+        for i in range(1, 10):
+            count[i] += count[i - 1]
+        i = len(lst) - 1
+        while i >= 0:
+            index = lst[i] // place
+            output[count[index % 10] - 1] = lst[i]
+            count[index % 10] -= 1
+            i -= 1
+        for i in range(len(lst)):
+            lst[i] = output[i]
+        place *= 10
+    return lst
+
+def heapify(lst, n, i):
+    largest, left, right = i, 2 * i + 1, 2 * i + 2
+    if left < n and lst[left] > lst[largest]:
+        largest = left
+    if right < n and lst[right] > lst[largest]:
+        largest = right
+    if largest != i:
+        lst[i], lst[largest] = lst[largest], lst[i]
+        heapify(lst, n, largest)
+    return
+def heap_sort(lst):
+    # Time Complexity
+    # Best               O(n*log n)
+    # Worst              O(n*log n)
+    # Average            O(n*log n)
+    # Space Complexity   O(1)
+    # Stability	         NO
+    res = []
+    n = len(lst)
+    for i in range(n // 2, -1, -1):
+        heapify(lst, n, i)
+    for i in range(n - 1, -1, -1):
+        lst[0], lst[i] = lst[i], lst[0]
+        res.insert(0, lst[i])
+        heapify(lst, i, 0)
+    return res
+
+def shell_sort(lst):
+    interval = len(lst) // 2
+    while interval > 0:
+        for i in range(interval, len(lst)):
+            temp = lst[i]
+            j = i
+            while j >= interval and lst[j - interval] > temp:
+                lst[j] = lst[j - interval]
+                j -= interval
+            lst[j] = temp
+        interval //= 2
+    return lst
+
+list = [121, 432, 564, 23, 1, 45, 788]
+print(shell_sort(list))
